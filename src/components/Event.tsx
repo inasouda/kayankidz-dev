7279c3109bd6404b790b70cde5bcdbebde8264e5
Inas Ouda <inasouda@Inass-Laptop.local>
2024-09-12 13:07:00 -0600

    change events scrolling imgs

--- src/components/Event.tsx
+++ src/components/Event.tsx
@@ -1,7 +1,6 @@
-import { Box, Text, Image, SimpleGrid, Flex,
-      useDisclosure, Modal, ModalOverlay, ModalContent,
-      ModalCloseButton, ModalBody } from "@chakra-ui/react";
+import { Box, Text, Image, Flex} from "@chakra-ui/react";
 import React from "react";
+import Slider from "react-slick";
 
 interface EventProps {
   title: string;
@@ -11,16 +10,20 @@ interface EventProps {
 }
 
 const Event: React.FC<EventProps> = ({ title, description, images, bgcolor }) => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const [selectedImage, setSelectedImage] = React.useState<string | null>(null);
-
-  const handleImageClick = (src: string) => {
-    setSelectedImage(src);
-    onOpen();
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+   
   };
+
 
   return (
     <Box
+      w={{ base: "100%", md: "80%", lg: "80%" }} 
       p="1rem"
       m={{ base: "1rem 0", md: "2rem 10rem", lg:"2rem 10rem"}} 
       textAlign="center"
@@ -29,84 +32,66 @@ const Event: React.FC<EventProps> = ({ title, description, images, bgcolor }) =>
       borderRadius="1.5rem"
     >
       <Flex
-        direction={{ base: "column", lg: "row" }} 
-        align="center"
-        justify="center"
-        gap={{ base: "4", lg: "8" }}
-      >
-        <Box
-          flex="1"
-          textAlign={{ base: "center", lg: "left" }} 
-          p={4}
+          direction={{ base: "column", lg: "row" }} 
+          align="center"
+          justify="center"
+          gap={{ base: "4", lg: "8" }}
         >
-          <Text  fontSize="xl" fontWeight={'bold'} mb={4}>
-            {title}
-          </Text>
-          <Text fontSize="md" mb={4}>
-            {description}
-          </Text>
-        </Box>
-        <Box
-          flex="1"
-          w={{ base: "full", lg: "50%" }}
-          p={4}
-          maxH="31.25rem" 
-          overflowY="auto"
-          bgColor="#F0F0F0"
-          borderRadius="md"
-          border="1px solid #ccc" 
-          boxShadow="md"
-          position="relative" 
-        >
-
           <Box
-            position="absolute"
-            top="0"
-            right="0"
-            bottom="0"
-            left="0"
-            pointerEvents="none"
-            bgGradient="linear(to-r, transparent, rgba(0, 0, 0, 0.2))"
-            display={{ base: "none", lg: "block" }}
-          />
-          <SimpleGrid 
-            columns={{ base: 2, md: 2, lg: 3 }}
-            spacing={4}
+            flex="1"
+            textAlign={{ base: "center", lg: "left" }} 
+            p={4}
           >
-            {images.map((img, index) => (
-              <Image
-                key={index}
-                src={img}
-                alt={`Event Image ${index + 1}`}
-                boxSize={{ base: "9.375rem", md: "12.5rem", lg: "15.625rem" }} 
-                borderRadius="md"
-                boxShadow="md"
-                objectFit="cover"
-                cursor="pointer"
-                onClick={() => handleImageClick(img)}
-              />
-            ))}
-          </SimpleGrid>
-        </Box>
+            <Text  fontSize="xl" fontWeight={'bold'} mb={4}>
+              {title}
+            </Text>
+            <Text fontSize="md" mb={4}>
+              {description}
+            </Text>
+          </Box>
+          <Box
+            flex="1"
+            w={{ base: "full", lg: "50%" }}
+            p={4}
+            maxH="31.25rem" 
+            overflowY="auto"
+            bgColor="#F0F0F0"
+            borderRadius="md"
+            border="1px solid #ccc" 
+            boxShadow="md"
+            position="relative" 
+          >
+            <Box
+              position="absolute"
+              top="0"
+              right="0"
+              bottom="0"
+              left="0"
+              pointerEvents="none"
+              bgGradient="linear(to-r, transparent, rgba(0, 0, 0, 0.2))"
+              display={{ base: "none", lg: "flex" }}
+              justifyContent={'center'}
+              alignItems={'center'}
+            />
+            <Box mb={"1.5rem"}>
+              <Slider {...settings}>
+                {
+                  images.map((img, index) => (
+                    <Box key={index}>
+                      <Image
+                        src={img}
+                        borderRadius="md"
+                        width="100%"
+                        height="25rem"
+                        objectFit="cover"
+                      />
+                    </Box>
+                  ))
+                }
+              </Slider>
+            </Box>
+          </Box>
       </Flex>
-
-      <Box display="flex" justifyContent="center" alignItems="center" height="50%">
-
-
-      {selectedImage && (
-        <Modal isOpen={isOpen} onClose={onClose} isCentered>
-          <ModalOverlay />
-          <ModalContent>
-            <ModalCloseButton />
-            <ModalBody p={0}>
-              <Image src={selectedImage} alt="Enlarged view" />
-            </ModalBody>
-          </ModalContent>
-        </Modal>
-       )}
-
-        </Box>
-
     </Box>
   );
 };
